b15da106830df1589aa215db561ee5aab3dc7852
josech <josech0191@gmail.com>
2018-04-06 23:15:12 -0500

    express passport list users

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-var express = require('express')
-var app = express()
+// server.js
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var express  = require('express');
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var app      = express();
+var port     = process.env.PORT || 8080;
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var passport = require('passport');
+var flash    = require('connect-flash');
+
+require('./config/passport')(passport);
+
+
+
+app.use(morgan('dev')); 
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+
+app.use(session({
+	secret: 'vidyapathaisalwaysrunning',
+	resave: true,
+	saveUninitialized: true
+ } )); 
+app.use(passport.initialize());
+app.use(passport.session()); 
+app.use(flash()); 
+
+
+require('./app/routes.js')(app, passport);
+
+app.listen(port);
+console.log('The magic happens on port ' + port);
---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'mcldisu5ppkm29wf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        'user': 'a0n0usohqi7p5yl5',
+        'password': 'feq9u9caui7g62zt'
+    },
+	'database': 'pz171o121ruc1yc6',
+    'users_table': 'users'
+};
